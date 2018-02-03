7ad966c3b6b3ce89e1335f86a2709aa7a286f118
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2018-02-03 18:17:30 +0000

    Created class based views and their urls.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 
 urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     path('', views.PostListView.as_view(), name='post_list'),
-
+    re_path('post/(?P<pk>\d+)', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
+    re_path('post/(?P<pk>\d+)/edit/', views.PostUpdateView.as_view(),
+         name='post_edit'),
+    re_path('post/(?P<pk>\d+)/remove/', views.PostDeleteView.as_view(),
+         name='post_remove'),
+    path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
 ]
