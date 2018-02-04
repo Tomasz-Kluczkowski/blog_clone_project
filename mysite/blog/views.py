7f86cc932f8bd1fbec5c3fa36cda688692fcd162
Tomasz Kluczkowski <tomaszk1@hotmail.co.uk>
2018-02-04 14:55:56 +0000

    Added code to blog.css Created base.html template. Created login.html template. Finished urls and views.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -74,3 +74,27 @@ def add_comment_to_post(request, pk):
         return render(request, 'blog/comment_form.html', {'form': form})
 
 
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    redirect('post_detail', pk=pk)
+
+
+
+
